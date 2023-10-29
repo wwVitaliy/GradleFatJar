c9f69c437a827a9350e607a71f091e7d052787e2
Vitalii Shliakhetko <wwvitaliy@gmail.com>
2023-10-29 09:43:37 +0200

    Initial commit

---
+++ src/main/java/org/example/UserName.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class UserName {
+    String name;
+    String lastName;
+
+    public UserName(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+}
